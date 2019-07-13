c32ae15d5336a83f48079a56f9115a8e91eb1ccb
nimish <nimish4july1998@gmail.com>
2019-07-13 16:20:30 +0530

    testing

--- scouts/models.py
+++ scouts/models.py
@@ -395,6 +395,7 @@ def scout_task_assignment_request_post_save_hook(sender, instance, created, **kw
         try:
             send_date = timezone.now() + timedelta(minutes=2)
             scout_assignment_request_set_rejected.apply_async([instance.id], eta=send_date)
+            sentry_debug_logger.error('sending after 2 minutes', exc_info=True)
         except Exception as E:
             sentry_debug_logger.error('error is ' + str(E), exc_info=True)
 
