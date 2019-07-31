6852c7c7c8bc2fdf05663c5c5cf2f812bd1e42da
nimish <nimish4july1998@gmail.com>
2019-07-31 13:17:58 +0530

    registered model admin

--- scouts/sub_tasks/admin.py
+++ scouts/sub_tasks/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
 
 # Register your models here.
+from scouts.sub_tasks.models import MoveOutRemark, MoveOutAmenitiesCheckup
+
+
+@admin.register(MoveOutRemark)
+class MoveOutRemarkAdmin(admin.ModelAdmin):
+    list_display = ('id', 'content', 'task', 'parent_task_category', 'parent_subtask_category')
+    list_filter = ('parent_task_category', 'parent_subtask_category', 'task__status')
+
+
+@admin.register(MoveOutAmenitiesCheckup)
+class AmenityCheckupModelAdmin(admin.ModelAdmin):
+    list_display = ('id', 'amenities_json', 'task', 'parent_task_category', 'parent_subtask_category')
+    list_filter = ('parent_task_category', 'parent_subtask_category', 'task__status')
