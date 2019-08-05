ab134137dd575a951d6559ec175d08a41fec7ce7
nimish <nimish4july1998@gmail.com>
2019-08-05 14:23:04 +0530

    minor fix

--- scouts/api/views.py
+++ scouts/api/views.py
@@ -486,14 +486,15 @@ class ScoutConsumerLinkAndScoutTaskCreateView(GenericAPIView):
             property_on_board_task_category = ScoutTaskCategory.objects.get(name=PROPERTY_ONBOARDING)
             from scouts.sub_tasks.models import PropertyOnBoardingDetail
             try:
-                property_onboarding_details = PropertyOnboardingDetailSerializer(data=data).save()
-                # property_onboarding_details = PropertyOnBoardingDetail.objects.create(
-                #     name=data['name'], phone_no=data['phone_no'], location=data.get('location'),
-                #     latitude=data['latitude'], longitude=data['longitude'], scheduled_time=data['scheduled_time'])
+                property_onboarding_details_serializer = PropertyOnboardingDetailSerializer(data=data)
+                if property_onboarding_details_serializer.is_valid():
+                    property_onboarding_details = property_onboarding_details_serializer.save()
+                    # property_onboarding_details = PropertyOnBoardingDetail.objects.create(
+                    #     name=data['name'], phone_no=data['phone_no'], location=data.get('location'),
+                    #     latitude=data['latitude'], longitude=data['longitude'], scheduled_time=data['scheduled_time'])
+                    scheduled_at = property_onboarding_details.scheduled_at
             except Exception as E:
                 raise ValidationError({STATUS: ERROR, 'detail': str(E)})
-
-            scheduled_at = property_onboarding_details.scheduled_at
 
             scout_task = ScoutTask.objects.create(category=property_on_board_task_category,
                                                   scheduled_at=scheduled_at,
